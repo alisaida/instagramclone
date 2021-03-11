99109b3b8b3fa25f1ddeeba7d7d1f47fee1e48e1
alisaid <40726986+si3iid@users.noreply.github.com>
2021-03-11 22:31:33 +1000

    finished feed and bottom nav

--- components/ProfilePicture/styles.js
+++ components/ProfilePicture/styles.js
@@ -3,13 +3,13 @@ import { StyleSheet } from 'react-native'
 const styles = StyleSheet.create({
   container: {
     margin: 5,
-    borderRadius: 40,
+    borderRadius: 50,
     borderWidth: 3,
     borderColor: "#a61985"
   },
   image: {
-    borderRadius: 40,
-    borderWidth: 1.5,
+    borderRadius: 50,
+    borderWidth: 2.5,
     borderColor: "#ffffff"
   }
 })
