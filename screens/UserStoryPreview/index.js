3b283032fa1971966974987474753fded4a7e52c
alisaid <40726986+si3iid@users.noreply.github.com>
2021-03-12 01:30:46 +1000

    user stories view and nav

---
+++ screens/UserStoryPreview/index.js
@@ -0,0 +1,11 @@
+import React from 'react';
+import { View, Text, SafeAreaView } from 'react-native';
+
+
+const UserStoryPreview = () => (
+    <SafeAreaView>
+        <Text>Wasa Wasa Wasa Wasss uuuuuuuuppppppp</Text>
+    </SafeAreaView>
+);
+
+export default UserStoryPreview;
