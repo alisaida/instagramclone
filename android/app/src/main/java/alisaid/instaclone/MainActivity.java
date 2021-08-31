43ff9641333215ead9b1339f60c68baef8b113ad
alisaid <40726986+si3iid@users.noreply.github.com>
2021-09-01 00:36:40 +1000

    renamed app

--- android/app/src/main/java/alisaid/instaclone/MainActivity.java
+++ android/app/src/main/java/alisaid/instaclone/MainActivity.java
@@ -1,4 +1,4 @@
-package com.instaclone;
+package alisaid.instaclone;
 
 import com.facebook.react.ReactActivity;
 
@@ -10,6 +10,6 @@ public class MainActivity extends ReactActivity {
    */
   @Override
   protected String getMainComponentName() {
-    return "instaclone";
+    return "Instagram";
   }
 }
--- ios/InstagramTests/InstagramTests.m
+++ ios/InstagramTests/InstagramTests.m
@@ -7,11 +7,11 @@
 #define TIMEOUT_SECONDS 600
 #define TEXT_TO_LOOK_FOR @"Welcome to React"
 
-@interface instacloneTests : XCTestCase
+@interface InstagramTests : XCTestCase
 
 @end
 
-@implementation instacloneTests
+@implementation InstagramTests
 
 - (BOOL)findSubviewInView:(UIView *)view matching:(BOOL(^)(UIView *view))test
 {
